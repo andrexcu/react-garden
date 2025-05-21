54dcd455e16fef53db3cb88e48cc826eabd620c1
andrexcu <andrexcu@gmail.com>
2025-05-21 23:22:47 +0800

    added tailwind and routing

---
+++ src/challenges/tictactoe/Tictactoe.tsx
@@ -0,0 +1,9 @@
+const Tictactoe = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-cyan-700 text-center">
+      W TICTACTOE
+    </h1>
+  );
+};
+
+export default Tictactoe;
